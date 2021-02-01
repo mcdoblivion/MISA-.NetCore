9e5b4ed835427f659cd871530d3b046212e29a69
mcd_oblivion <mcd.oblivion@gmail.com>
2021-02-01 12:55:22 +0700

    Create WebAPI project

---
+++ MISA.CukCuk/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MISA.CukCuk.Models
+{
+    public class Customer
+    {
+        #region Property
+
+        public Guid CustomerId { get; set; }
+
+        public string CustomerCode { get; set; }
+
+        public string CustomerName { get; set; }
+
+        #endregion Property
+    }
+}
